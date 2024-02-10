fa38750430a2123c2169a5391ba17e9eef351a0f
John Mangel <john.m5321@yahoo.com>
2024-02-09 19:57:09 -0500

    Add ErrorPage errorElement

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Root from './routes/root'
 import { RouteObject, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import ErrorPage from './pages/ErrorPage';
 
 const App = () => {
   const routes: RouteObject[] = [
     {
       path: '/',
-      element: <Root />
+      element: <Root />,
+      errorElement: <ErrorPage />,
     }
   ];
 
