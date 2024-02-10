1e5abac9ef9bc49a4bfc8ba573140e5da19c4e42
John Mangel <john.m5321@yahoo.com>
2024-02-10 14:12:58 -0500

    Rename 'Send Me a Tip' to 'Tip The Dev'

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -14,7 +14,7 @@ export default function Root() {
               <Nav.Link as={Link} className="me-2" eventKey="/" to="/">Home</Nav.Link>
               <Nav.Link as={Link} className="me-2" eventKey="/monte_carlo" to="/monte_carlo">Monte Carlo</Nav.Link>
               <Nav.Link as={Link} className="me-2" to="mailto:songscaler+dev-feeback-pretire@gmail.com" target="_blank">Feedback</Nav.Link>
-              <Nav.Link as={Link} className="me-2" to="https://venmo.com/u/JohnMangel" target="_blank">Send Me a Tip (thanks! ＼(´∀｀)／)</Nav.Link>
+              <Nav.Link as={Link} className="me-2" to="https://venmo.com/u/JohnMangel" target="_blank">Tip The Dev (thanks! ＼(´∀｀)／)</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
