1a3e56692ab97b0e664084b948d7a8abee699548
John Mangel <john.m5321@yahoo.com>
2024-02-10 13:49:32 -0500

    Style active nav link

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,16 +1,18 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 
 export default function Root() {
+  const location = useLocation();
+
   return (
     <>
       <Navbar bg="light" expand="md">
         <Container>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link as={Link} className="me-2" to="/">Home</Nav.Link>
-              <Nav.Link as={Link} className="me-2" to="/monte_carlo">Monte Carlo</Nav.Link>
+            <Nav variant="underline" activeKey={location.pathname} className="me-auto">
+              <Nav.Link as={Link} className="me-2" eventKey="/" to="/">Home</Nav.Link>
+              <Nav.Link as={Link} className="me-2" eventKey="/monte_carlo" to="/monte_carlo">Monte Carlo</Nav.Link>
               <Nav.Link as={Link} className="me-2" to="https://venmo.com/u/JohnMangel" target="_blank">Send Me a Tip (thanks! ＼(´∀｀)／)</Nav.Link>
               <Nav.Link as={Link} className="me-2" to="mailto:songscaler+dev-feeback-pretire@gmail.com" target="_blank">Feedback</Nav.Link>
             </Nav>
