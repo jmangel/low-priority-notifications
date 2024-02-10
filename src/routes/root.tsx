35cfce74c4ffcc32fe3dc79bd948271f535aaa53
John Mangel <john.m5321@yahoo.com>
2024-02-10 13:39:16 -0500

    Add feedback email link

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -4,14 +4,15 @@ import { Link, Outlet } from "react-router-dom";
 export default function Root() {
   return (
     <>
-      <Navbar bg="light" expand="sm">
+      <Navbar bg="light" expand="md">
         <Container>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link as={Link} className="me-2" to="/">Home</Nav.Link>
               <Nav.Link as={Link} className="me-2" to="/monte_carlo">Monte Carlo</Nav.Link>
-              <Nav.Link as={Link} className="me-2" to="https://venmo.com/u/JohnMangel" target="_blank" >Send Me a Tip (thanks! ＼(´∀｀)／)</Nav.Link>
+              <Nav.Link as={Link} className="me-2" to="https://venmo.com/u/JohnMangel" target="_blank">Send Me a Tip (thanks! ＼(´∀｀)／)</Nav.Link>
+              <Nav.Link as={Link} className="me-2" to="mailto:songscaler+dev-feeback-pretire@gmail.com" target="_blank">Feedback</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
