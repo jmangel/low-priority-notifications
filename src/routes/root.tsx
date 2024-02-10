7d426d312222ca1026df7e5bc6b7d7f6c9056f7c
John Mangel <john.m5321@yahoo.com>
2024-02-09 19:38:17 -0500

    Setup basic Navbar in Root

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,7 +1,17 @@
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 export default function Root() {
   return (
-    <>
-      Hello World
-    </>
+    <Navbar bg="light">
+      <Container>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} className="me-2" to="/">Home</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
