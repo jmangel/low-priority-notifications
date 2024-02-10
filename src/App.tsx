3daee66ac59fcc49f1301efa2a0db1f7ca67eb39
John Mangel <john.m5321@yahoo.com>
2024-02-09 19:34:30 -0500

    Create browser router and Root

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import Root from './routes/root'
+import { RouteObject, RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App"></div>
-  );
+const App = () => {
+  const routes: RouteObject[] = [
+    {
+      path: '/',
+      element: <Root />
+    }
+  ];
+
+  return <RouterProvider router={createBrowserRouter(routes)} />
 }
 
 export default App;
