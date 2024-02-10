36e064466470d04f884a8dbb295ffdd83334b45a
John Mangel <john.m5321@yahoo.com>
2024-02-10 13:50:17 -0500

    Reorder Feedback link before Venmo link

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -13,8 +13,8 @@ export default function Root() {
             <Nav variant="underline" activeKey={location.pathname} className="me-auto">
               <Nav.Link as={Link} className="me-2" eventKey="/" to="/">Home</Nav.Link>
               <Nav.Link as={Link} className="me-2" eventKey="/monte_carlo" to="/monte_carlo">Monte Carlo</Nav.Link>
-              <Nav.Link as={Link} className="me-2" to="https://venmo.com/u/JohnMangel" target="_blank">Send Me a Tip (thanks! ＼(´∀｀)／)</Nav.Link>
               <Nav.Link as={Link} className="me-2" to="mailto:songscaler+dev-feeback-pretire@gmail.com" target="_blank">Feedback</Nav.Link>
+              <Nav.Link as={Link} className="me-2" to="https://venmo.com/u/JohnMangel" target="_blank">Send Me a Tip (thanks! ＼(´∀｀)／)</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
