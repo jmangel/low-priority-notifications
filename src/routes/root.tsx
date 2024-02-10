f7d7c79b9c1b6be75fda3063ef9d261c68a5fc42
John Mangel <john.m5321@yahoo.com>
2024-02-09 19:52:24 -0500

    Add link to venmo for tips

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -3,12 +3,13 @@ import { Link } from "react-router-dom";
 
 export default function Root() {
   return (
-    <Navbar bg="light">
+    <Navbar bg="light" expand="sm">
       <Container>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link as={Link} className="me-2" to="/">Home</Nav.Link>
+            <Nav.Link as={Link} className="me-2" to="https://venmo.com/u/JohnMangel" target="_blank" >Send Me a Tip (thanks! ＼(´∀｀)／)</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
